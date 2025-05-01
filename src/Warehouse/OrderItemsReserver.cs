80457f9d9adbbb12dd672b43371803f272fb2158
Aliaksei Dubadzel <aliaksei_dubadzel@epam.com>
2025-05-01 04:32:54 +0200

    Add OrderItemsReserver func / #4

---
+++ src/Warehouse/OrderItemsReserver.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Warehouse;
+
+public record Order(int OrderId, OrderItem[] OrderItems);
+public record OrderItem(int CatalogItemId, int Quantity);
+
+public class OrderItemsReserver
+{
+    [Function(nameof(OrderItemsReserver))]
+    [BlobOutput("order-items-reservation/{OrderId}")]
+    public OrderItem[] Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req, [FromBody] Order order)
+    {
+        // TODO validate, transform
+        return order.OrderItems;
+    }
+}
